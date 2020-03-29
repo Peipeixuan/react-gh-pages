f65cb25fcf07e2968e9d4d3be0f6462bf8ca3101
Peixuan <chintan5327@gmail.com>
2020-03-29 16:00:34 +0800

    Updates

---
+++ precache-manifest.9ffbda24b77d380b21c7009cfc6f28c8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01dff8e00ad0dbd3de0b39bb6e13087b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "53d85ff093082680adf4",
+    "url": "/react-gh-pages/static/css/main.f7a5d57a.chunk.css"
+  },
+  {
+    "revision": "475fc22338837bdecfee",
+    "url": "/react-gh-pages/static/js/2.dbea9213.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-gh-pages/static/js/2.dbea9213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d85ff093082680adf4",
+    "url": "/react-gh-pages/static/js/main.d8d08c2d.chunk.js"
+  },
+  {
+    "revision": "c910589a90c7db292e97",
+    "url": "/react-gh-pages/static/js/runtime-main.8d2d02b4.js"
+  },
+  {
+    "revision": "3b6443cf431f19bda7d6b9141f274e4f",
+    "url": "/react-gh-pages/static/media/2072492.3b6443cf.png"
+  },
+  {
+    "revision": "c07f4885e96b07bc65d20d1b8cc0a2fc",
+    "url": "/react-gh-pages/static/media/calculator.c07f4885.jpg"
+  },
+  {
+    "revision": "a8f8cf468329f14644ae28764e97e5b7",
+    "url": "/react-gh-pages/static/media/canvas.a8f8cf46.jpg"
+  },
+  {
+    "revision": "2058056791143138a112998fa8c17ffa",
+    "url": "/react-gh-pages/static/media/card.20580567.jpg"
+  },
+  {
+    "revision": "5a06afb58513c8d59ed7539999c5ab61",
+    "url": "/react-gh-pages/static/media/gogoro.5a06afb5.jpg"
+  },
+  {
+    "revision": "85610d316062514e3080828984007e04",
+    "url": "/react-gh-pages/static/media/name.85610d31.svg"
+  },
+  {
+    "revision": "7594cf1cd8edb3d6eb8d4b57670da083",
+    "url": "/react-gh-pages/static/media/netflix.7594cf1c.jpg"
+  },
+  {
+    "revision": "a5bb88d38c49fd3ac97745bfbff440ad",
+    "url": "/react-gh-pages/static/media/pms.a5bb88d3.jpg"
+  },
+  {
+    "revision": "df84ea2c4fa14643be3cf79e2c0b0870",
+    "url": "/react-gh-pages/static/media/primaryPhoto.df84ea2c.svg"
+  }
+]);
